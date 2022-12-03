0fe742561a805a08f766b8dc62713ba1a0476c29
Flerka <flerka@suddengunater.com>
2022-12-03 11:53:20 +0000

    add day 3

---
+++ day3.py
@@ -0,0 +1,33 @@
+from collections import Counter
+import string
+
+
+def read_lines(file):
+    content = file.read()
+    return [[group[:len(group) // 2], group[len(group) // 2:]] for group in content.split("\n")]
+
+
+def get_sum(chars):
+    return sum([(string.ascii_letters.index(char) + 1) for char in chars])
+
+
+def get_chars_pt1(file):
+    content = open(file, 'r').read()
+    lines = [[group[:len(group) // 2], group[len(group) // 2:]] for group in content.split("\n")]
+    return [(Counter(line[0]) & Counter(line[1]).keys()) for line in lines]
+
+
+def get_chars_pt2(file):
+    content = open(file, 'r').read()
+    lines = content.split("\n")
+    groups = [lines[i:i + 3] for i in range(0, len(lines), 3)]
+    return [(Counter(line[0]) & Counter(line[1]) & Counter(line[2])).keys() for line in groups]
+
+
+# first part
+inters_pt1 = get_chars_pt1("day3.txt")
+print(sum([get_sum(chars) for chars in inters_pt1]))
+
+# second part
+inters_pt2 = get_chars_pt2("day3.txt")
+print(sum([get_sum(chars) for chars in inters_pt2]))
